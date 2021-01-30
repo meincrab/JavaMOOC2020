4cc30fcb7275ea752e41c8f95bb4f6e2ef2f3eb1
Meincrab <meincrab@gmail.com>
2021-01-30 15:20:00 +0200

    sm0ll update

--- Ohjelmoinnin_perusteet/osa07-Osa07_07.Reseptihaku/src/main/java/Reseptihaku.java
+++ Ohjelmoinnin_perusteet/osa07-Osa07_07.Reseptihaku/src/main/java/Reseptihaku.java
@@ -2,12 +2,24 @@
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.nio.file.Paths;
 
 public class Reseptihaku {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
-
+        System.out.println("Mistä luetaan? ");
+        String tiedosto = lukija.nextLine();
+        ArrayList<Resepti> reseptit =
+        
     }
-
+    
+    public lueReseptitTiedostosta(String tiedostonNimi){
+        ArrayList<Resepti> reseptit = new ArrayList<>();
+        ArrayList<String> reseptinOsat = new ArrayList<>();
+        String reseptinNimi;
+        int reseptinAika;
+        try(Scanner tiedostonLukija = new Scanner(Paths))
+        
+    }
 }
