30e91bc5367bb11efbfd239f7f2c826af2956c29
Meincrab <meincrab@gmail.com>
2021-02-01 01:29:49 +0200

    ..

--- Ohjelmoinnin_perusteet/osa07-Osa07_07.Reseptihaku/src/main/java/Reseptihaku.java
+++ Ohjelmoinnin_perusteet/osa07-Osa07_07.Reseptihaku/src/main/java/Reseptihaku.java
@@ -10,16 +10,43 @@ public class Reseptihaku {
         Scanner lukija = new Scanner(System.in);
         System.out.println("Mistä luetaan? ");
         String tiedosto = lukija.nextLine();
-        ArrayList<Resepti> reseptit =
+        ArrayList<Resepti> reseptit = lueReseptitTiedostosta(tiedosto);
+        
         
     }
-    
-    public lueReseptitTiedostosta(String tiedostonNimi){
+
+    public static ArrayList<Resepti> lueReseptitTiedostosta(String tiedostonNimi) {
         ArrayList<Resepti> reseptit = new ArrayList<>();
         ArrayList<String> reseptinOsat = new ArrayList<>();
         String reseptinNimi;
         int reseptinAika;
-        try(Scanner tiedostonLukija = new Scanner(Paths))
-        
+        try ( Scanner tiedostonLukija = new Scanner(Paths.get(tiedostonNimi))) {
+            while (tiedostonLukija.hasNextLine()) {
+                String rivi = tiedostonLukija.nextLine();
+                if (!rivi.isEmpty()) {
+                    reseptinOsat.add(rivi);
+                } else {
+                    reseptinNimi = reseptinOsat.get(0);
+                    reseptinAika = Integer.valueOf(reseptinOsat.get(1));
+                    reseptinOsat.remove(0);
+                    reseptinOsat.remove(0);
+                    reseptit.add(new Resepti(reseptinNimi, reseptinAika, reseptinOsat));
+                    reseptinOsat.clear();
+                }
+            }
+            if (!reseptinOsat.isEmpty()) {
+                reseptinNimi = reseptinOsat.get(0);
+                reseptinAika = Integer.valueOf(reseptinOsat.get(1));
+                reseptinOsat.remove(0);
+                reseptinOsat.remove(0);
+                reseptit.add(new Resepti(reseptinNimi, reseptinAika, reseptinOsat));
+                reseptinOsat.clear();
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        return reseptit;
+
     }
 }
