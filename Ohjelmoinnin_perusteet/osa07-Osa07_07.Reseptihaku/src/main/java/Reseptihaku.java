bd2b972f53effd36f5cf9059ccde50b4540a5fd4
Meincrab <meincrab@gmail.com>
2021-02-01 23:51:14 +0200

    goes on

--- Ohjelmoinnin_perusteet/osa07-Osa07_07.Reseptihaku/src/main/java/Reseptihaku.java
+++ Ohjelmoinnin_perusteet/osa07-Osa07_07.Reseptihaku/src/main/java/Reseptihaku.java
@@ -11,10 +11,31 @@ public class Reseptihaku {
         System.out.println("Mistä luetaan? ");
         String tiedosto = lukija.nextLine();
         ArrayList<Resepti> reseptit = lueReseptitTiedostosta(tiedosto);
-        
-        
+        System.out.println("Komennot:\n"
+                + "listaa - listaa reseptit\n"
+                + "lopeta - lopettaa ohjelman");
+        while(true){
+            System.out.print("Syötä komento: ");
+            String komento = lukija.nextLine();
+            System.out.println("");
+            if(komento.equalsIgnoreCase("lopeta")){
+                break;
+            }
+            lueKomento(komento, reseptit, lukija);
+        }
     }
-
+    public static void lueKomento(String komento, ArrayList<Resepti> reseptit, Scanner syotto){
+        if(komento.equalsIgnoreCase("listaa")){
+            tulostaReseptit(reseptit);
+            System.out.println("");
+        }
+    }
+    public static void tulostaReseptit(ArrayList<Resepti> reseptit){
+        System.out.println("Reseptit: ");
+        for(Resepti resepti : reseptit){
+            System.out.println(resepti.toString());
+        }
+    }
     public static ArrayList<Resepti> lueReseptitTiedostosta(String tiedostonNimi) {
         ArrayList<Resepti> reseptit = new ArrayList<>();
         ArrayList<String> reseptinOsat = new ArrayList<>();
