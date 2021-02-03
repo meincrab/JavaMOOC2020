dd9db88dae0ca32d31395c6304451c993243019f
Meincrab <meincrab@gmail.com>
2021-02-03 02:09:14 +0200

    Reseptihaku

--- Ohjelmoinnin_perusteet/osa07-Osa07_07.Reseptihaku/src/main/java/Reseptihaku.java
+++ Ohjelmoinnin_perusteet/osa07-Osa07_07.Reseptihaku/src/main/java/Reseptihaku.java
@@ -11,31 +11,84 @@ public class Reseptihaku {
         System.out.println("Mistä luetaan? ");
         String tiedosto = lukija.nextLine();
         ArrayList<Resepti> reseptit = lueReseptitTiedostosta(tiedosto);
-        System.out.println("Komennot:\n"
+        System.out.println(
+                "Komennot:\n"
                 + "listaa - listaa reseptit\n"
-                + "lopeta - lopettaa ohjelman");
-        while(true){
+                + "lopeta - lopettaa ohjelman\n"
+                + "hae nimi - hakee reseptiä nimen perusteella\n"
+                + "hae keittoaika - hakee reseptiä keittoajan perusteella\n"
+                + "hae aine - hakee reseptiä raaka-aineen perusteella");
+
+        while (true) {
             System.out.print("Syötä komento: ");
             String komento = lukija.nextLine();
             System.out.println("");
-            if(komento.equalsIgnoreCase("lopeta")){
+            if (komento.equalsIgnoreCase("lopeta")) {
                 break;
             }
             lueKomento(komento, reseptit, lukija);
         }
     }
-    public static void lueKomento(String komento, ArrayList<Resepti> reseptit, Scanner syotto){
-        if(komento.equalsIgnoreCase("listaa")){
+
+    public static void lueKomento(String komento, ArrayList<Resepti> reseptit, Scanner syotto) {
+        if (komento.equalsIgnoreCase("listaa")) {
             tulostaReseptit(reseptit);
             System.out.println("");
         }
+        if (komento.equalsIgnoreCase("hae nimi")) {
+            tulostaReseptiNimella(reseptit, syotto);
+            System.out.println("");
+        }
+        if (komento.equalsIgnoreCase("hae keittoaika")) {
+            tulostaReseptiKeittoajalla(reseptit, syotto);
+            System.out.println("");
+        }
+        if (komento.equalsIgnoreCase("hae aine")) {
+            tulostaReseptiAineella(reseptit, syotto);
+            System.out.println("");
+        }
     }
-    public static void tulostaReseptit(ArrayList<Resepti> reseptit){
+
+    public static void tulostaReseptiAineella(ArrayList<Resepti> reseptit, Scanner lukija) {
+        System.out.println("Mitä raaka-ainetta haetaan: ");
+        String sana = lukija.nextLine();
+        System.out.println("Reseptit:");
+        for (Resepti resepti : reseptit) {
+            if (resepti.getAineosat().contains(sana)) {
+                System.out.println(resepti.toString());
+            }
+        }
+    }
+
+    public static void tulostaReseptiNimella(ArrayList<Resepti> reseptit, Scanner lukija) {
+        System.out.print("Mitä haetaan: ");
+        String sana = lukija.nextLine();
+        System.out.print("Reseptit:");
+        for (Resepti resepti : reseptit) {
+            if (resepti.getNimi().contains(sana)) {
+                System.out.println(resepti.toString());
+            }
+        }
+    }
+
+    public static void tulostaReseptiKeittoajalla(ArrayList<Resepti> reseptit, Scanner lukija) {
+        System.out.println("Keittoaika korkeintaan:");
+        int aika = Integer.valueOf(lukija.nextLine());
+        System.out.println("Reseptit:");
+        for (Resepti resepti : reseptit) {
+            if (resepti.getAika() <= aika) {
+                System.out.println(resepti.toString());
+            }
+        }
+    }
+
+    public static void tulostaReseptit(ArrayList<Resepti> reseptit) {
         System.out.println("Reseptit: ");
-        for(Resepti resepti : reseptit){
+        for (Resepti resepti : reseptit) {
             System.out.println(resepti.toString());
         }
     }
+
     public static ArrayList<Resepti> lueReseptitTiedostosta(String tiedostonNimi) {
         ArrayList<Resepti> reseptit = new ArrayList<>();
         ArrayList<String> reseptinOsat = new ArrayList<>();
