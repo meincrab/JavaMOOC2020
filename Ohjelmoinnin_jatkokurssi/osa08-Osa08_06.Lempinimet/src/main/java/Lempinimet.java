7f74a112bb52f47fc44980f2318a5d0f97ec2500
Meincrab <meincrab@gmail.com>
2021-02-06 14:33:21 +0200

    08-06

---
+++ Ohjelmoinnin_jatkokurssi/osa08-Osa08_06.Lempinimet/src/main/java/Lempinimet.java
@@ -0,0 +1,14 @@
+
+import java.util.HashMap;
+
+public class Lempinimet {
+
+    public static void main(String[] args) {
+        // Tee tehtävänannossa pyydetyt operaatiot täällä!
+        HashMap<String, String> lempinimet = new HashMap<>();
+        lempinimet.put("Matti", "Mage");
+        lempinimet.put("Mikael", "Mixu");
+        lempinimet.put("Arto",  "Arppa");
+    }
+
+}
