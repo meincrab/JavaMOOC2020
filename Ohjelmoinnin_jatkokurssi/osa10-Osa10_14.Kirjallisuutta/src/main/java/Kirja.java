296db901c81b6c20e2465937fecd664830054d89
Meincrab <meincrab@gmail.com>
2021-05-07 23:25:32 +0300

    chapter 10 task 14

--- Ohjelmoinnin_jatkokurssi/osa10-Osa10_14.Kirjallisuutta/src/main/java/Kirja.java
+++ Ohjelmoinnin_jatkokurssi/osa10-Osa10_14.Kirjallisuutta/src/main/java/Kirja.java
@@ -12,5 +12,21 @@ public class Kirja {
     private String nimi;
     private int kohdeIka;
     
+    public Kirja(String nimi, int kohdeIka){
+        this.nimi = nimi;
+        this.kohdeIka = kohdeIka;
+    }
     
+    
+    public String getNimi(){
+        return this.nimi;
+    }
+    public int getKohdeIka(){
+        return this.kohdeIka;
+    }
+    
+    @Override
+    public String toString(){
+        return this.nimi + " (" + this.kohdeIka + "  vuotiaille ja vanhemmille)";
+    }    
 }
--- Ohjelmoinnin_jatkokurssi/osa10-Osa10_14.Kirjallisuutta/src/main/java/Paaohjelma.java
+++ Ohjelmoinnin_jatkokurssi/osa10-Osa10_14.Kirjallisuutta/src/main/java/Paaohjelma.java
@@ -1,11 +1,33 @@
 
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Collections;
 
 public class Paaohjelma {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
-
+        List<Kirja> kirjat = new ArrayList<>();
+        while (true) {
+            System.out.print("Syötä kirjan nimi, tyhjä lopettaa: ");
+            String nimi = lukija.nextLine();
+            if (nimi.isEmpty()) {
+                break;
+            }
+            System.out.print("Syötä kirjan pienin kohdeikä: ");
+            String ika = lukija.nextLine();
+            kirjat.add(new Kirja(nimi, Integer.valueOf(ika)));
+            System.out.println("");
+        }
+        System.out.println("\nYhteensä " + kirjat.size() + " kirjaa");
+        System.out.println("\nKirjat:");
+        Comparator<Kirja> vertaileIkajaNimi = Comparator
+                .comparing(Kirja::getKohdeIka)
+                .thenComparing(Kirja::getNimi);
+        Collections.sort(kirjat, vertaileIkajaNimi);
+        kirjat.stream().forEach(System.out::println);
     }
 
 }
